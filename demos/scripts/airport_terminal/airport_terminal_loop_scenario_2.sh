a02d9c3b8b05a9d0d33c302eca04503f04bc969d
Aaron Chong <aaronchongth@gmail.com>
2020-03-23 18:55:47 +0800

    Feature/doors (#42)
    * added double hinged doors at strategic locations
    
    * testing double hinged door on office, adding new robot behind doors scenario
    
    * corrected office building test
    
    * added lanes, changed 3rd mir loop route to pass through doors, manually added wall segments for doors
    
    * Ensure lanes overlap
    
    * Added two new loop scenarios
    
    * Install loop scenario files
    
    * Modified position of junction_n02
    
    Co-authored-by: Yadunund <yadunund@openrobotics.org>

--- demos/scripts/airport_terminal/airport_terminal_loop_scenario_2.sh
+++ demos/scripts/airport_terminal/airport_terminal_loop_scenario_2.sh
@@ -21,34 +21,24 @@ function send_loop_job {
 
 echo "Spawning and sending job to robot mir100_0."
 spawn_robot MiR100 mir100_0
-send_loop_job junction_central_west junction_north_west mir100
-
+send_loop_job junction_south_west junction_south_east mir100
 
 echo "Spawning and sending job to robot mir100_1."
 spawn_robot MiR100 mir100_1
-send_loop_job junction_central_east junction_north_east mir100
-
-
-echo "Spawning and sending job to robot mir100_2."
-spawn_robot MiR100 mir100_2
-send_loop_job west_koi_pond junction_n01 mir100
-
+send_loop_job junction_north_west junction_south_west mir100
 
 echo "Spawning and sending job to robot magni_0."
 spawn_robot Magni magni_0
-send_loop_job junction_s10 junction_n18 magni
-
+send_loop_job n08 magni_n09 magni
 
 echo "Spawning and sending job to robot magni_1."
 spawn_robot Magni magni_1
-send_loop_job junction_s11 junction_n20 magni
-
+send_loop_job n12 n13 magni
 
 echo "Spawning and sending job to robot magni_2."
 spawn_robot Magni magni_2
-send_loop_job junction_s16 junction_n28 magni
-
+send_loop_job junction_n18 s10 magni
 
 echo "Spawning and sending job to robot magni_3."
 spawn_robot Magni magni_3
-send_loop_job junction_s24 junction_n32 magni
+send_loop_job n24_0 junction_s13 magni
