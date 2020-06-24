392a9288eb8899d48e0cb5e953dd94d561524f11
Aaron Chong <aaronchongth@gmail.com>
2020-06-24 14:38:50 +0800

    Develop (#85)
    * Updated request_delivery with pickup and dropoff dispener names
    
    * Fixed dangling waypoint
    
    * Added newly migrated Door panels
    
    * Reverted some automatic changes when updating configs
    
    * Adding marker subscriber for /building_systems_markers
    
    * Fixes for first release (#84)
    
    * Removed workcell names from maps and added delivery launch files
    
    * Updated RMF panel with new Delivery message structure. Updated rviz configs
    
    * Linted with pycodestyle
    
    * Updated build.yaml
    
    * RMF linter
    
    * Fixed EOL error
    
    * Updated README.md
    
    Co-authored-by: Yadunund <yadunund@openrobotics.org>

--- rmf_demo_tasks/rmf_demo_tasks/request_delivery.py
+++ rmf_demo_tasks/rmf_demo_tasks/request_delivery.py
@@ -8,29 +8,28 @@ import rclpy
 
 from rmf_task_msgs.msg import Delivery
 
-def main(argv = sys.argv):
+
+def main(argv=sys.argv):
     rclpy.init(args=argv)
     args_without_ros = rclpy.utilities.remove_ros_args(argv)
 
-    '''
-    # Example request:
-    task_id: randomid_001
-    items: [itemA, itemB....]
-    pickup_place_name: cssd_room
-    pickup_behavior:
-    - name: dispenser
-    - parameters: [request_guid: xxx, target_guid:cssdbot, transporter_type:mir]
-    dropoff_place_name: ot_prep_room
-    dropoff_behavior:
-    - name: dispenser
-    - parameters: [request_guid: yyy, target_guid:otbot, transporter_type:mir]
-    '''
-
     parser = argparse.ArgumentParser()
-    parser.add_argument('-p', '--pickup', default='pantry', help='Start waypoint')
-    parser.add_argument('-d', '--dropoff', default='hardware_2', help='Finish waypoint')
-    parser.add_argument('-i', '--task-id', help='Task ID', default='', type=str)
-    parser.add_argument('-r', '--robot-type', help='Type of robot', default='magni')
+    parser.add_argument('-p',
+                        '--pickup', default='pantry', help='Start waypoint')
+    parser.add_argument('-pd',
+                        '--pickup_dispenser',
+                        default='coke_dispenser',
+                        help='Pickup dispenser name')
+    parser.add_argument('-d',
+                        '--dropoff',
+                        default='hardware_2', help='Finish waypoint')
+    parser.add_argument('-dd',
+                        '--dropoff_dispenser',
+                        default='coke_ingestor', help='Dropoff dispenser name')
+    parser.add_argument('-i',
+                        '--task-id', help='Task ID', default='', type=str)
+    parser.add_argument('-r',
+                        '--robot-type', help='Type of robot', default='magni')
 
     args = parser.parse_args(args_without_ros[1:])
 
@@ -46,15 +45,17 @@ def main(argv = sys.argv):
         request.task_id = 'delivery#' + str(uuid.uuid1())
     request.pickup_place_name = args.pickup
     request.dropoff_place_name = args.dropoff
+    request.pickup_dispenser = args.pickup_dispenser
+    request.dropoff_dispenser = args.dropoff_dispenser
 
     for _ in range(5):
         publisher.publish(request)
         sleep(0.5)
-        
+
     rclpy.shutdown()
 
-
     print(f'Delivery request submitted to {args.robot_type}')
+
 
 if __name__ == '__main__':
     main(sys.argv)
