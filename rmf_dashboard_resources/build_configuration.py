0118554035a46ac3fb75db25c057c6aa68b38f2e
Matias Bavera <matiasbavera@gmail.com>
2020-08-10 15:53:45 -0400

    Removed name of world of the Json file

--- rmf_dashboard_resources/build_configuration.py
+++ rmf_dashboard_resources/build_configuration.py
@@ -2,19 +2,20 @@ import glob, os
 import json
 from pathlib import Path
 
-maps = ['office', 'airport_terminal']
+# Get the directories names inside the folder rmf_dashboard_resources
+worlds = next(os.walk('.'))[1]
 
-for map in maps:
+for world in worlds:
     app_config = {
         "dispensers":{},
         "robots":{}
     }
-    for topic in app_config.keys():    
-        # Get all JSON files inside the folder of each MAP
-        files = Path(map + '/' + topic + '/').glob("**/*.json")
+    for topic in app_config.keys():
+        # Get all JSON files inside the folder of each world
+        files = Path(world + '/' + topic + '/').glob("**/*.json")
         for file in files:
             with open(file) as json_file:
-                app_config[topic].update(json.load(json_file)) 
-    
-    with open(map + '/main.json', 'w') as main_file:
-        main_file.write(json.dumps({map:app_config}))
+                app_config[topic].update(json.load(json_file))
+
+    with open(world + '/main.json', 'w') as main_file:
+        main_file.write(json.dumps(app_config))
