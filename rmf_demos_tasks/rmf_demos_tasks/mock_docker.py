6380f7d6058eda4ff706333f3fbd901466cae611
Xi Yu Oh <xiyu@openrobotics.org>
2021-12-01 03:04:03 +0800

    added lag time before dock summary is published
    Signed-off-by: Xi Yu Oh <xiyu@openrobotics.org>

--- rmf_demos_tasks/rmf_demos_tasks/mock_docker.py
+++ rmf_demos_tasks/rmf_demos_tasks/mock_docker.py
@@ -18,6 +18,7 @@ import rclpy
 import math
 import argparse
 import yaml
+import time
 from rclpy.node import Node
 from rclpy.time import Time
 
@@ -116,6 +117,7 @@ class MockDocker(Node):
                 dock_sub_map[dock_name] = param.path
             dock_summary.docks.append(dock)
             self.dock_map[fleet_name] = dock_sub_map
+        time.sleep(20)
         self.dock_summary_publisher.publish(dock_summary)
 
     def mode_request_cb(self, msg: ModeRequest):
