7f9606afd65f535e8e5e3f866b01aed8ba910708
Xiyu <xiyu@openrobotics.org>
2022-09-06 13:23:43 +0800

    Minor fixes for demo fleet adapter (#156)
    * Append current waypoint to PathRequest for start_task
    * Fix sleep for in RobotCommandHandle dock
    * Fix f-string
    
    Signed-off-by: Xi Yu Oh <xiyu@openrobotics.org>

--- rmf_demos_fleet_adapter/rmf_demos_fleet_adapter/RobotCommandHandle.py
+++ rmf_demos_fleet_adapter/rmf_demos_fleet_adapter/RobotCommandHandle.py
@@ -364,8 +364,8 @@ class RobotCommandHandle(adpt.RobotCommandHandle):
                     self.name, self.dock_name, self.map_name)):
                 self.node.get_logger().info(
                     f"Requesting robot {self.name} "
-                    "to dock at {self.dock_name}")
-                time.sleep_for(1.0)
+                    f"to dock at {self.dock_name}")
+                self.sleep_for(1.0)
 
             with self._lock:
                 self.on_waypoint = None
