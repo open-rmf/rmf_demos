390165fdcf3abc6fb36e9fb2fffd467367e521b6
Xi Yu Oh <xiyu@openrobotics.org>
2022-11-07 11:10:57 +0800

    Roscon workshop updates
    Signed-off-by: Xi Yu Oh <xiyu@openrobotics.org>

--- rmf_demos_fleet_adapter/rmf_demos_fleet_adapter/easy_fleet_adapter.py
+++ rmf_demos_fleet_adapter/rmf_demos_fleet_adapter/easy_fleet_adapter.py
@@ -1,4 +1,4 @@
-# Copyright 2022 Open Source Robotics Foundation, Inc.
+# Copyright 2021 Open Source Robotics Foundation, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -17,12 +17,24 @@ import argparse
 import yaml
 import time
 import threading
+import datetime
 
 import rclpy
 import rclpy.node
 from rclpy.parameter import Parameter
 
 import rmf_adapter as adpt
+import rmf_adapter.plan as plan
+
+from .configuration import get_configuration
+
+from functools import partial
+
+from rclpy.qos import QoSProfile
+from rclpy.qos import QoSHistoryPolicy as History
+from rclpy.qos import QoSDurabilityPolicy as Durability
+from rclpy.qos import QoSReliabilityPolicy as Reliability
+from rclpy.qos import qos_profile_system_default
 
 from .RobotClientAPI import RobotAPI
 
@@ -31,121 +43,100 @@ from .RobotClientAPI import RobotAPI
 # ------------------------------------------------------------------------------
 
 
-def add_easy_fleet_robots(node, easy_adapter, config_yaml):
-    # Initialize robot API for this fleet
-    fleet_config = config_yaml['rmf_fleet']
-    prefix = 'http://' + fleet_config['fleet_manager']['ip'] + \
-             ':' + str(fleet_config['fleet_manager']['port'])
-    api = RobotAPI(
-        prefix,
-        fleet_config['fleet_manager']['user'],
-        fleet_config['fleet_manager']['password'])
+class FleetAdapter:
 
-    # List of robot names
-    missing_robots = config_yaml['robots']
+    def __init__(self, config_yaml, nav_graph_path, node, use_sim_time):
+        # global counter for command ids
+        self.next_id = 0
+        # Keep track of which map the robot is in
+        self.last_map = {}
+        self.cmd_ids = {}
+        # Initialize robot API for this fleet
+        fleet_config = config_yaml['rmf_fleet']
+        prefix = 'http://' + fleet_config['fleet_manager']['ip'] + \
+                ':' + str(fleet_config['fleet_manager']['port'])
+        self.api = RobotAPI(
+            prefix,
+            fleet_config['fleet_manager']['user'],
+            fleet_config['fleet_manager']['password'])
 
-    # Assign callbacks to each robot of the fleet
-    def _assign_callbacks(robot_name: str, start_pose: list):
+        configuration = get_configuration(config_yaml, nav_graph_path, node)
+        self.adapter = self.initialize_fleet(configuration, config_yaml['robots'], node, use_sim_time)
 
-        # Define get_position callback for this robot
-        def _get_position():
-            current_status = api.data(robot_name)
-            if not current_status:
-                return
-            pose = current_status['data']['position']
-            battery_soc = current_status['data']['battery']
-            map_name = current_status['data']['map_name']
-            replan = api.requires_replan(robot_name)
+    def initialize_fleet(self, configuration, robots_yaml, node, use_sim_time):
+        # Make the easy full control
+        easy_full_control = adpt.EasyFullControl.make(configuration)
 
-            position = adpt.easy_full_control_handle.Position()
-            position.position = [pose['x'], pose['y'], pose['yaw']]
-            position.battery_percent = battery_soc
-            position.map_name = map_name
-            position.replan = replan
-            return position
+        if use_sim_time:
+            easy_full_control.node.use_sim_time()
 
-        # Use this callback to check whether the robot completed process
-        def _check_completed(cmd_id: int):
-            resp = api.process_completed(robot_name, cmd_id)
-            return resp
+        def _goal_completed(robot_name, remaining_time, request_replan):
+            request_replan = self.api.requires_replan(robot_name)
+            remaining_time = self.api.navigation_remaining_duration(robot_name, self.cmd_ids[robot_name])
+            return self.api.process_completed(robot_name, self.cmd_ids[robot_name])
 
-        # Define navigate callback for this robot
-        def _navigate(target: adpt.easy_full_control_handle.Target):
-            resp = api.navigate(robot_name, target.cmd_id, target.pose,
-                                target.map_name, target.speed_limit)
-            if resp:
-                return _check_completed
-            else:
-                return
+        def _robot_state(robot_name):
+            data = self.api.data(robot_name)
+            if data is None or data['success'] is False:
+                return None
+            pos = data['data']['position']
+            state = adpt.easy_full_control.RobotState(
+                robot,
+                robot_config['charger']['waypoint'],
+                data['data']['map_name'],
+                [pos['x'], pos['y'], pos['yaw']],
+                data['data']['battery'])
+            self.last_map[robot_name] = data['data']['map_name']
+            return state
 
-        # Define dock callback for this robot
-        def _dock(dock_name: str, cmd_id: int):
-            resp = api.start_process(
-                robot_name, cmd_id, dock_name, map_name='')
-            if resp:
-                return _check_completed
-            else:
-                return
+        def _navigate(robot_name, map_name, goal, update_handle):
+            cmd_id = self.next_id
+            self.next_id += 1
+            self.cmd_ids[robot_name] = cmd_id
+            self.api.navigate(robot_name, cmd_id, goal, map_name)
+            node.get_logger().info(f"Navigating robot {robot_name}")
+            return partial(_goal_completed, robot_name)
 
-        # Define stop function for this robot
-        def _stop(cmd_id: int):
-            resp = api.stop(robot_name, cmd_id)
-            return resp
+        def _stop(robot_name):
+            cmd_id = self.next_id
+            self.next_id += 1
+            self.cmd_ids[robot_name] = cmd_id
+            return self.api.stop(robot_name, cmd_id)
 
-        # Define action executor callback for this robot
-        def _action_executor(category: str,
+        def _dock(robot_name, dock_name, update_handle):
+            cmd_id = self.next_id
+            self.next_id += 1
+            self.cmd_ids[robot_name] = cmd_id
+            self.api.start_process(robot_name, cmd_id, dock_name, self.last_map[robot_name])
+            return partial(_goal_completed, robot_name)
+
+        def _action_executor(robot_name: str,
+                             category: str,
                              description: dict,
-                             execution:
-                             adpt.robot_update_handle.ActionExecution):
-            pass  # do nothing for now
+                             execution: adpt.robot_update_handle.ActionExecution):
+            pass
 
-        success = easy_adapter.add_robot(robot_name,
-                                         start_pose,
-                                         _get_position,
-                                         _navigate,
-                                         _dock,
-                                         _stop,
-                                         _action_executor)
-        return success
-
-    def _add_fleet_robots():
-        while len(missing_robots) > 0:
-            time.sleep(0.2)
-
-            for robot_name in list(missing_robots.keys()):
-                node.get_logger().debug(f'Connecting to robot: {robot_name}')
-                data = api.data(robot_name)
-                if data is None:
+        # Add the robots
+        for robot in robots_yaml:
+            node.get_logger().info(f'Found robot {robot}')
+            robot_config = robots_yaml[robot]['rmf_config']
+            success = False
+            while success is False:
+                state = _robot_state(robot)
+                if state is None:
+                    time.sleep(0.2)
                     continue
-                if data['success']:
-                    # We use the robot's current position to compute plan start
-                    position = api.position(robot_name)
-                    if position is None:
-                        node.get_logger().info(
-                            f'Failed to get initial position of {robot_name}')
-                        continue
-                    start_pose = \
-                        [position[0], position[1], position[2]]
+                success = True
+                # Add robot to fleet
+                easy_full_control.add_robot(
+                    state,
+                    partial(_robot_state, robot),
+                    partial(_navigate, robot),
+                    partial(_stop, robot),
+                    partial(_dock, robot),
+                    partial(_action_executor, robot))
 
-                    success = _assign_callbacks(robot_name, start_pose)
-
-                    if success:
-                        node.get_logger().info(
-                            f'Successfully added new robot: {robot_name}')
-                    else:
-                        node.get_logger().error(
-                            f'Failed to initialize robot: {robot_name}')
-
-                    del missing_robots[robot_name]
-
-                else:
-                    node.get_logger().debug(
-                        f'{robot_name} not found, trying again...')
-
-        return
-
-    add_robots = threading.Thread(target=_add_fleet_robots, args=())
-    add_robots.start()
+        return easy_full_control
 
 
 # ------------------------------------------------------------------------------
@@ -185,33 +176,11 @@ def main(argv=sys.argv):
         param = Parameter("use_sim_time", Parameter.Type.BOOL, True)
         node.set_parameters([param])
 
-    # Set server uri
-    node.declare_parameter('server_uri', rclpy.Parameter.Type.STRING)
-    server_uri = node.get_parameter(
-        'server_uri').get_parameter_value().string_value
-    if server_uri == "":
-        server_uri = None
-
-    # Make an adapter instance
-    adapter = adpt.Adapter.make(f'{fleet_name}_fleet_adapter')
-    if args.use_sim_time:
-        adapter.node.use_sim_time()
-    assert adapter, ("Unable to initialize fleet adapter. Please ensure "
-                     "RMF Schedule Node is running")
-    adapter.start()
-    time.sleep(1.0)
-
-    # Set up Configuration and make EasyFullControl fleet
-    adapter_config = adpt.easy_full_control_handle.Configuration(
-        config_path,
+    adapter = FleetAdapter(
+        config_yaml,
         nav_graph_path,
-        server_uri)
-
-    easy_adapter = adpt.EasyFullControl.make(adapter_config, adapter)
-    assert easy_adapter, ("Unable to initialize easy fleet adapter.")
-
-    # Add robots to fleet adapter
-    add_easy_fleet_robots(node, easy_adapter, config_yaml)
+        node,
+        args.use_sim_time)
 
     # Create executor for the command handle node
     rclpy_executor = rclpy.executors.SingleThreadedExecutor()
