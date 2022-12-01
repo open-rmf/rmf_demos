b144571edbb91bdc49c69060179109216972a6b3
Teo Koon Peng <koonpeng@openrobotics.org>
2022-12-01 12:03:39 +0800

    vendor fastapi and uvicorn
    Signed-off-by: Teo Koon Peng <koonpeng@openrobotics.org>

---
+++ rmf_demos_fleet_adapter/rmf_demos_fleet_adapter/__init__.py
@@ -0,0 +1,7 @@
+import os
+import sys
+
+
+sys.path.insert(
+    0, f"{os.path.dirname(__file__)}/_vendor"
+)
--- rmf_demos_fleet_adapter/setup.py
+++ rmf_demos_fleet_adapter/setup.py
@@ -1,8 +1,24 @@
 import os
+from shutil import rmtree
+from subprocess import check_call
+
 from glob import glob
 from setuptools import setup, find_packages
+from distutils.command.build import build as _build
 
 package_name = 'rmf_demos_fleet_adapter'
+vendor_pkgs = ['fastapi==0.79.0', 'uvicorn==0.18.2']
+here = os.path.dirname(__file__)
+
+
+class build(_build):
+    def run(self):
+        vendor_dir = f'{here}/{package_name}/_vendor'
+        rmtree(vendor_dir, ignore_errors=True)
+        check_call(('pip3', 'install', '-I', '-t', vendor_dir, *vendor_pkgs))
+        with open(f'{vendor_dir}/__init__.py', 'w'):
+            pass
+        super().run()
 
 setup(
     name=package_name,
@@ -15,9 +31,7 @@ setup(
         ('share/' + package_name, ['config.yaml']),
         (os.path.join('share', package_name, 'launch'),
             glob('launch/*.launch.xml')),
-
     ],
-    install_requires=['setuptools', 'fastapi>=0.79.0', 'uvicorn>=0.18.2'],
     zip_safe=True,
     maintainer='Xi Yu Oh',
     maintainer_email='xiyu@openrobotics.org',
@@ -31,4 +45,5 @@ setup(
             'fleet_manager=rmf_demos_fleet_adapter.fleet_manager:main',
         ],
     },
+    cmdclass={'build': build},
 )
