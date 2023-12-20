75281d26011093a9d33575f49c9434c4a00d1450
Yadu <yadunund@openrobotics.org>
2023-12-20 13:21:12 +0800

    Bump main to 2.2.3 (#199)
    * Update changelogs
    
    Signed-off-by: Yadunund <yadunund@openrobotics.org>
    
    * 2.2.3
    
    Signed-off-by: Yadunund <yadunund@openrobotics.org>
    
    ---------
    
    Signed-off-by: Yadunund <yadunund@openrobotics.org>

--- rmf_demos_fleet_adapter/setup.py
+++ rmf_demos_fleet_adapter/setup.py
@@ -6,7 +6,7 @@ package_name = 'rmf_demos_fleet_adapter'
 
 setup(
     name=package_name,
-    version='2.2.2',
+    version='2.2.3',
     packages=find_packages(),
     data_files=[
         ('share/ament_index/resource_index/packages',
--- rmf_demos_tasks/setup.py
+++ rmf_demos_tasks/setup.py
@@ -4,7 +4,7 @@ package_name = 'rmf_demos_tasks'
 
 setup(
     name=package_name,
-    version='2.2.2',
+    version='2.2.3',
     packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
