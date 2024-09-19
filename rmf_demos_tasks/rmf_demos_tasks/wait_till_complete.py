2c55b5c9425ebe5809d4b6c5c02ae5447d5bdbe6
Arjo Chakravarty <arjoc@google.com>
2024-09-19 13:16:49 +0800

    Fix style
    Signed-off-by: Arjo Chakravarty <arjoc@google.com>

--- rmf_demos_tasks/rmf_demos_tasks/wait_till_complete.py
+++ rmf_demos_tasks/rmf_demos_tasks/wait_till_complete.py
@@ -53,7 +53,7 @@ class TaskObserver(Node):
             return
         for robot_state in fleet_state.robots:
             if robot_state.name == self.parser.robot \
-                and robot_state.task_id == "":
+            and robot_state.task_id == "":
                 self.response.set_result(robot_state)
                 return
 
