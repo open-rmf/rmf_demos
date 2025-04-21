248e8e327711a1751a3f624e8f77785a693a4116
Arjo Chakravarty <arjo@openrobotics.org>
2025-04-21 15:57:24 +0800

    Adds a script to cancel the currently executing task on a specific robot. (#286)
    Signed-off-by: Arjo Chakravarty <arjoc@google.com>
    (cherry picked from commit 7f929590eac7d38149dfb31de10cd34933a7d871)

--- rmf_demos_tasks/setup.py
+++ rmf_demos_tasks/setup.py
@@ -27,6 +27,7 @@ setup(
             'request_loop = rmf_demos_tasks.request_loop:main',
             'request_lift = rmf_demos_tasks.request_lift:main',
             'cancel_task = rmf_demos_tasks.cancel_task:main',
+            'cancel_robot_task = rmf_demos_tasks.cancel_robot_task:main',
             'dispatch_loop = rmf_demos_tasks.dispatch_loop:main',
             'dispatch_action = rmf_demos_tasks.dispatch_action:main',
             'dispatch_patrol = rmf_demos_tasks.dispatch_patrol:main',
